82d42dc87a746ca2e08bab5d4170fe39f54c1614
yollotltamayo <yollotltamayo@comunidad.unam.mx>
2021-08-16 09:40:12 -0500

    agrega _Documents.js _app.js Cesar.jsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import { ThemeProvider, createTheme } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const theme = createTheme({
+        palette: {
+            type: "dark",
+        }
+    })
+    React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+      </ThemeProvider>
+  )
 }
 export default MyApp
+
